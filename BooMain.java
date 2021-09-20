1ea3028035c9d9b7ac2abfc8820c9964f5348cc5
RebeccaF99 <90312243+RebeccaF99@users.noreply.github.com>
2021-09-20 16:11:19 +0100

    Boolean Java Test
    testing boolean commands

---
+++ BooMain.java
@@ -0,0 +1,10 @@
+public class BooMain {
+
+  public static void main(String[] args) {
+  boolean isJavaFun = true;
+  boolean isFishTasty = false;
+  System.out.println(isJavaFun); //Outputs true
+  System.out.println(isFishTasty); //Outputs false
+  
+  }
+}
